9407f6e3c76020d5ede33ba6f308dfb24013720c
irockmywarlock <noemirsevilla@gmail.com>
2020-09-03 21:55:41 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace multidimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [,,] multiplication_table = new int [9, 1, 9];
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i + "\t");
+                for (int j = 2; j <=10; j++)
+                {
+                    Console.Write(i * j + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
